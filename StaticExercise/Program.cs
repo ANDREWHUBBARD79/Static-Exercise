6e74bee760073eb753769bc4429162619a572d36
Andrew <andrewhubbard78@gmail.com>
2023-09-29 13:05:51 -0400

    A

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace StaticExercise
+﻿using System.Net.WebSockets;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+
+
+            var celcius = TempConverter.CelciusToFahrenheit(68);//20
+            var fahrenheit = TempConverter.FahrenheitToCelcius(68);//68
+            
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
         }
     }
 }
